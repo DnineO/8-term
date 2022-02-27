2900ef7814710157a426096d60ab80795f487380
DnineO <yakadzunvano@mail.ru>
2022-02-27 15:46:33 +0900

    Create lab 3 polygram.py

---
+++ Защита информации/labs/lab 3 polygram.py
@@ -0,0 +1,11 @@
+import string
+
+
+alphabet = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
+            'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', '_']
+
+print(alphabet)
+
+alphabet = list(string.ascii_uppercase)
+
+print(alphabet)
