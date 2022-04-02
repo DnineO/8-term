8b4e1345088599b1ffbda69f446f5b72f1305e00
DnineO <yakadzunvano@mail.ru>
2022-04-02 14:11:06 +0900

    Update lab 5 stream.py

--- Защита информации/labs/lab 5 stream.py
+++ Защита информации/labs/lab 5 stream.py
@@ -22,21 +22,44 @@ def generation_random_numbers():
     return gen
 
 
-def gamma(message, gen):
+def create_gamma(message, gen):
+    temp = gen
+    # print(temp)
+
     i, j = 0, 0
     for k in range(len(message)):
         i = (i + 1) % 256
+        j = (j + temp[i]) % 256
+        temp[i], temp[j] = temp[j], temp[i]
+        t = (temp[i] + temp[j]) % 256
+        gamma.append(temp[t])
+    return gamma
+
+
+def num2bin(num,pos):
+
+
+
+def encrypt(text):
+    gamm = create_gamma(text, gen)
+    bytes = []
 
 
 def main():
     print("запуск")
     generate_key()  # creating key and shuffle
 
+    print("Enter message: ")
+    message = input()
+
     # print("1", key)
     # print("2", gen)
 
     # generation_random_numbers()
     # print(gen)
+
+    # print("",create_gamma(message, gen))
+
 
 
 main()
